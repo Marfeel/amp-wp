d1ef710742cb2f56aef3d01d99c674b74cb013a8
eduardcot <eduard.cot@marfeel.com>
2018-05-23 16:12:12 +0200

    twitter_embed

--- includes/embeds/class-amp-twitter-embed.php
+++ includes/embeds/class-amp-twitter-embed.php
@@ -13,6 +13,13 @@
 class AMP_Twitter_Embed_Handler extends AMP_Base_Embed_Handler {
 	const URL_PATTERN = '#http(s|):\/\/twitter\.com(\/\#\!\/|\/)([a-zA-Z0-9_]{1,20})\/status(es)*\/(\d+)#i';
 
+	protected $sanitize_tag = 'blockquote';
+
+	protected $DEFAULT_WIDTH = 600;
+	protected $DEFAULT_HEIGHT = 600;
+
+	private $amp_tag = 'amp-twitter';
+
 	public function register_embed() {
 		add_shortcode( 'tweet', array( $this, 'shortcode' ) );
 		wp_embed_register_handler( 'amp-twitter', self::URL_PATTERN, array( $this, 'oembed' ), -1 );
@@ -72,4 +79,76 @@ class AMP_Twitter_Embed_Handler extends AMP_Base_Embed_Handler {
 
 		return $this->shortcode( array( 'tweet' => $id ) );
 	}
+
+	/**
+	 * Sanitized <blockquote class="instagram-media"> tags to <amp-instagram>
+	 *
+	 * @param DOMDocument $dom DOM.
+	 */
+	public function sanitize_raw_embeds( $dom ) {
+		/**
+		 * Node list.
+		 *
+		 * @var DOMNodeList $node
+		 */
+		$nodes     = $dom->getElementsByTagName( $this->sanitize_tag );
+		$num_nodes = $nodes->length;
+
+		if ( 0 === $num_nodes ) {
+			return;
+		}
+
+		for ( $i = $num_nodes - 1; $i >= 0; $i-- ) {
+			$node = $nodes->item( $i );
+			if ( ! $node instanceof DOMElement ) {
+				continue;
+			}
+
+			if ( $this->is_twitter_raw_embed( $node ) ) {
+				
+				$this->create_amp_twitter_and_replace_node( $dom, $node );
+			}
+		}
+	}
+
+	/**
+	 * Make final modifications to DOMNode
+	 *
+	 * @param DOMNode $node The DOMNode to adjust and replace.
+	 *
+	 * @return boolean
+	 */
+	private function is_twitter_raw_embed( $node ) {
+		$class_attr = $node->getAttribute( 'class' );
+
+		return null !== $class_attr && false !== strpos( $class_attr, 'twitter-tweet' );
+
+	}
+
+	private function create_amp_twitter_and_replace_node( $dom, $node ) {
+		$twitter_id = $this->get_twitter_id( $node );
+
+		$new_node = AMP_DOM_Utils::create_node( $dom, $this->amp_tag, array(
+				'data-tweetid' => $twitter_id,
+				'layout' => 'responsive',
+				'width' => $this->DEFAULT_WIDTH,
+				'height' => $this->DEFAULT_HEIGHT,
+		) );
+
+		$node->parentNode->replaceChild( $new_node, $node );
+
+		$this->did_convert_elements = true;
+	}
+
+	private function get_twitter_id( $node ) {
+		$anchors = $node->getElementsByTagName('a');
+		
+		foreach ( $anchors as $anchor ) {
+			$found = preg_match( self::URL_PATTERN, $anchor->getAttribute( 'href' ), $matches );
+			if ( $found ) {
+				return $matches[5];
+			}
+		}
+	}
+
 }
